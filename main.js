f930be95a14e5db543d76a9c8894f88881e50164
nimit2801 <mnimitsavant@outlook.com>
2024-03-03 21:43:09 +0530

    add static website

---
+++ main.js
@@ -0,0 +1,15 @@
+import './style.css';
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <section>
+      <div class="newsletter-box">
+        <h2>Scratch Newsletter</h2>
+        <form>
+          <input type="text" placeholder="Email" />
+          <button>Subscribe</button>
+        </form>
+      </div>
+    </section>
+  </div>
+`
